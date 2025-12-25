f9779c7b244fc9add76fce4b0d0325767560f573
RcubDev <rlinehan2@gmail.com>
2025-12-25 12:54:44 -0700

    add interface for toasts

--- Demo/addons/Godot.Achievements.Net/Toast/AchievementToastItem.cs
+++ Demo/addons/Godot.Achievements.Net/Toast/AchievementToastItem.cs
@@ -4,23 +4,10 @@ namespace Godot.Achievements.Toast;
 
 /// <summary>
 /// Default toast item for displaying achievement unlock notifications.
-/// Users can create custom toast scenes by implementing a Control with a Setup(Achievement) method.
+/// Users can create custom toast scenes by implementing <see cref="IAchievementToastItem"/>.
 /// </summary>
-/// <remarks>
-/// <para>
-/// To create a custom toast scene:
-/// 1. Create a new scene with a Control-based root node (e.g., PanelContainer)
-/// 2. Add a script that implements a public void Setup(Achievement achievement) method
-/// 3. Set the scene path in Project Settings > Addons > Achievements > Toast > Scene Path
-/// </para>
-/// <para>
-/// IMPORTANT: If you want to preview your custom toast in the editor using the "Visualize Unlock"
-/// button, your script MUST be marked with the [Tool] attribute. Without this attribute, the toast
-/// will only work at runtime.
-/// </para>
-/// </remarks>
 [Tool]
-public partial class AchievementToastItem : PanelContainer
+public partial class AchievementToastItem : PanelContainer, IAchievementToastItem
 {
     [Export]
     private TextureRect IconRect = null!;
